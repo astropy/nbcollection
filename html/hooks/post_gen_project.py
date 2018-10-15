ea115889745bd2ad876e579327dff739138c4d1e
Erik Tollerud <erik.tollerud@gmail.com>
2018-10-15 02:25:07 -0400

    add missing indent

--- html/hooks/post_gen_project.py
+++ html/hooks/post_gen_project.py
@@ -5,12 +5,11 @@ import shutil
 
 
 if '{{ cookiecutter.copy_nbpages }}'.lower() == 'y':
+    import nbpages
 
-import nbpages
+    nbpages_sourcedir = os.path.realpath(nbpages.__path__[0])
+    nbpages_targetdir = os.path.join(os.path.realpath(os.path.curdir), 'nbpages')
 
-nbpages_sourcedir = os.path.realpath(nbpages.__path__[0])
-nbpages_targetdir = os.path.join(os.path.realpath(os.path.curdir), 'nbpages')
-
-shutil.copytree(nbpages_sourcedir, nbpages_targetdir,
-                ignore=shutil.ignore_patterns('*.pyc', '.*', '__pycache__'))
+    shutil.copytree(nbpages_sourcedir, nbpages_targetdir,
+                    ignore=shutil.ignore_patterns('*.pyc', '.*', '__pycache__'))
 
