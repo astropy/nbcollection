b784ff2fa1c06e701b2ce7e442df9d7d12a30f08
Erik Tollerud <erik.tollerud@gmail.com>
2018-10-15 02:40:48 -0400

    add note of index generation

--- html/{{ cookiecutter.pages_name }}/convert.py
+++ html/{{ cookiecutter.pages_name }}/convert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import logging
 
 from nbpages import make_parser, run_parsed, make_html_index
 
@@ -8,4 +9,6 @@ args = make_parser().parse_args()
 if args.template_file is None and os.path.exists('nb_html.tpl'):
     args.template_file = 'nb_html.tpl'
 converted = run_parsed('.', output_type='HTML', args=args)
+
+logging.getLogger('nbpages').info('Generating index.html')
 make_html_index(converted, './index.tpl')
