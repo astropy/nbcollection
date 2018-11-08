d135a7d8a7b285a7e9a3b5c129f31bf638e28565
Erik Tollerud <erik.tollerud@gmail.com>
2018-11-08 02:34:54 -0500

    add option for index to use relative paths

--- nbpages/html_index.py
+++ nbpages/html_index.py
@@ -2,11 +2,17 @@ import os
 import jinja2
 
 
-def make_html_index(converted_files, html_template, outfn='index.html'):
+def make_html_index(converted_files, html_template, outfn='index.html',
+                    relpaths=True):
     path, fn = os.path.split(html_template)
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(path),
                              autoescape=jinja2.select_autoescape(['html', 'xml']))
     templ = env.get_template(fn)
+
+    if relpaths:
+        outdir = os.path.realpath(os.path.dirname(outfn))
+        converted_files = [os.path.relpath(os.path.realpath(pth), outdir)
+                           for pth in converted_files]
 
     with open(outfn, 'w') as f:
         f.write(templ.render(notebook_html_paths=converted_files))
