85896e664dc37c4cb2aa3d43593b02a8f4306afb
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-12-16 15:43:16 -0500

    modern setup

---
+++ nbstatic/__init__.py
@@ -0,0 +1,1 @@
+from .core import NBStaticConverter, NBStaticNotebook
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-from setuptools import setup, find_packages
-setup(use_scm_version=True, packages=find_packages())
+from setuptools import setup
+setup(use_scm_version=True)
