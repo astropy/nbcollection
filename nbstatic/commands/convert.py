9adfec7bc1fb53ff8bb032ef54b0b3d11695b26c
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-03-31 20:06:13 -0400

    add index cli flags

--- nbstatic/commands/convert.py
+++ nbstatic/commands/convert.py
@@ -15,10 +15,17 @@ def convert(args=None):
     parser.add_argument("--template", dest="template", default=None,
                         help="A jinja2 template file passed to nbconvert.")
 
+    parser.add_argument("--index-template", dest="index_template", default=None,
+                        help="A jinja2 template file used to create the index "
+                             "page.")
+
+    parser.add_argument("--make-index", dest="make_index", default=False,
+                        help="Controls whether to make an index page that "
+                             "lists all of the converted notebooks.")
+
     args = parser.parse_args(args[2:])
 
     kwargs = vars(args)
 
     nbstatic = NBStaticConverter(**kwargs)
-    nbstatic.execute()
     nbstatic.convert()
