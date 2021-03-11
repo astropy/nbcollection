7b6ba8e5785bc86cd6ef582df7c9cdfcae9df1d5
Joe Curtin <jcurtin@stsci.edu>
2021-03-11 14:22:38 -0500

    Updated deployment hash

--- nbcollection/ci/renderer/template/setup-env.sh
+++ nbcollection/ci/renderer/template/setup-env.sh
@@ -5,6 +5,7 @@ set -e
 export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
 
+apt-get update
 apt-get install -y git make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl \
     llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libfreetype6-dev
 
