3ddd82b0ac85067ccac451509011e469f4f070c1
Joe Curtin <jcurtin@stsci.edu>
2021-03-11 14:16:44 -0500

    Removed sudo

--- nbcollection/ci/renderer/template/setup-env.sh
+++ nbcollection/ci/renderer/template/setup-env.sh
@@ -5,10 +5,10 @@ set -e
 export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
 
-sudo apt-get install -y git
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm
-sudo apt-get install -y libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-sudo apt-get install -y libfreetype6-dev
+apt-get install -y git
+apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm
+apt-get install -y libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+apt-get install -y libfreetype6-dev
 
 git clone https://github.com/spacetelescope/nbcollection nbcollection
 cd nbcollection
