a85a62c8ba95db6fe1e772425c487ef70c59458e
Joe Curtin <jcurtin@stsci.edu>
2021-03-11 09:34:46 -0500

    Updated nbcollection-ci version to be installed

--- nbcollection/ci/renderer/template/setup-env.sh
+++ nbcollection/ci/renderer/template/setup-env.sh
@@ -7,7 +7,7 @@ export LC_ALL=C.UTF-8
 
 git clone https://github.com/spacetelescope/nbcollection nbcollection
 cd nbcollection
-git checkout ea33b6a72b55859da45728f79ebddc8975205f6d
+git checkout 09473aba85a78efd4cbd7a20e21b95dc9a9131cf
 pip install -U pip setuptools
 pip install -r ci_requirements.txt
 python setup.py install
